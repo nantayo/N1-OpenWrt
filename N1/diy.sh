7b682eaf191b7e5fc1be7b10ec16c1c7e45dccfd
nantayo <sora1924@outlook.com>
2023-10-17 09:36:45 +0800

    N1 OpenWrt

---
+++ N1/diy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Default IP
+sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
+
+# Firewall
+echo "iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE" >> package/network/config/firewall/files/firewall.user
+
+# Remove packages
+rm -rf feeds/luci/applications/luci-app-passwall feeds/packages/net/haproxy feeds/packages/net/v2ray-geodata
+
+# Add packages
+svn co https://github.com/nantayo/my-pkg/trunk package/my-pkg
+svn co https://github.com/ophub/luci-app-amlogic/trunk package/amlogic
