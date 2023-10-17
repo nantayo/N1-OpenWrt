a37c8e712bb4f2266141e48c8696a4c2eb5ad50d
youfun <junezpe@gmail.com>
2023-10-17 08:06:52 -0500

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
+
+## 解除系统限制
+ulimit -u 10000
+ulimit -n 4096
+ulimit -d unlimited
+ulimit -m unlimited
+ulimit -s unlimited
+ulimit -t unlimited
+ulimit -v unlimited
+
+
 # Default IP
-sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.0.3/g' package/base-files/files/bin/config_generate
+
+
 
 # Firewall
 echo "iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE" >> package/network/config/firewall/files/firewall.user
@@ -12,3 +25,9 @@ rm -rf feeds/luci/applications/luci-app-passwall feeds/packages/net/haproxy feed
 # Add packages
 svn co https://github.com/nantayo/my-pkg/trunk package/my-pkg
 svn co https://github.com/ophub/luci-app-amlogic/trunk package/amlogic
+git clone https://github.com/kenzok8/small-package package/small-package
+
+
+
+
+
