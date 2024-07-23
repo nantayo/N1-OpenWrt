8c9ee558506066eb7f9da2c744d6a7f4f66172ee
nantayo <sora1924@outlook.com>
2024-07-23 14:04:18 +0800

    N1 OpenWrt

---
+++ N1/diy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Default IP
+sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
+
+# Remove packages
+rm -rf feeds/luci/applications/luci-app-passwall
+
+# Add packages
+git clone https://github.com/nantayo/My-Pkg package/my-pkg
+git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/amlogic
