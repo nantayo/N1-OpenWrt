15e6e9041eb94a5d0bb8843823c10cd1b7e66c2e
nantayo <sora1924@outlook.com>
2025-03-22 20:37:57 +0800

    N1 OpenWrt

---
+++ N1/diy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Add packages
+git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
+git clone https://github.com/xiaorouji/openwrt-passwall --depth=1 clone/passwall
+
+# Update packages
+rm -rf feeds/luci/applications/luci-app-passwall
+cp -rf clone/amlogic/luci-app-amlogic clone/passwall/luci-app-passwall feeds/luci/applications/
+
+# Clean packages
+rm -rf clone
