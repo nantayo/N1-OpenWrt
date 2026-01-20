c4e52aae4f456161392291625b074bea10618f72
nantayo <sora1924@outlook.com>
2026-01-20 10:38:23 +0800

    N1 OpenWrt

---
+++ armsr/armv8/diy/diy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Adjust source code
+patch -p1 -f < $(dirname "$0")/luci.patch
+
+# Add packages
+git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
+git clone https://github.com/Openwrt-Passwall/openwrt-passwall --depth=1 clone/passwall
+
+# Update packages
+rm -rf feeds/luci/applications/luci-app-passwall
+cp -rf clone/amlogic/luci-app-amlogic clone/passwall/luci-app-passwall feeds/luci/applications/
+
+# Clean packages
+rm -rf clone
